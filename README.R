a13370194ccbd3c307b556b62efa9ecd3ea6529c
tsalter <nsalter@sfu.ca>
2017-07-12 14:16:28 -0700

    Initial commit

---
+++ README.R
@@ -0,0 +1,10 @@
+
+Description 
+=============
+
+I am tyring to predict whether patients have heart disease based on 13 clinical variables.
+
+Hypothesis 
+=============
+
+Heart disease status can be determined from other clinical variables
